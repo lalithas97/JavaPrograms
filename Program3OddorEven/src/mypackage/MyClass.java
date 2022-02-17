d918ba9fdc84c9616380dcd28df2180e850d01f2
lalithas97 <chspllsowmya@gmail.com>
2022-02-17 22:03:12 +0530

    Program 3 Check-In Even Odd Numbers

---
+++ Program3OddorEven/src/mypackage/MyClass.java
@@ -0,0 +1,19 @@
+package mypackage;
+
+public class MyClass {
+
+	public static void main(String[] args) 
+	{
+		int	i=23;
+		if(i%2==0)
+		{
+			System.out.println(i+ " is a even number");
+		}
+		else
+		{
+			System.out.println(i+ " is odd number");
+		}
+
+	}
+
+}
